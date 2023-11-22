11fb71d6524252f42c9cfe9cdf613647bf393391
Ali(Ako) Hosseini <ali.hosseini@deriv.com>
2023-11-22 15:24:10 +0800

    Revert "Shayan/P2PS-1753/refactor exchange rates hook (#11034)"
    This reverts commit f6820b8ab936759d400eef9fa78c2c5486ea5e81.

--- packages/hooks/src/useP2PAdvertList.ts
+++ packages/hooks/src/useP2PAdvertList.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import { usePaginatedFetch } from '@deriv/api';
+import useExchangeRate from './useExchangeRate';
 
 /**
  * This custom hook returns available adverts for use with 'p2p_order_create' by calling 'p2p_advert_list' endpoint
@@ -7,6 +8,7 @@ import { usePaginatedFetch } from '@deriv/api';
 const useP2PAdvertList = (
     payload?: NonNullable<Parameters<typeof usePaginatedFetch<'p2p_advert_list'>>[1]>['payload']
 ) => {
+    const { getRate } = useExchangeRate();
     const { data, ...rest } = usePaginatedFetch('p2p_advert_list', { payload });
 
     // Add additional information to the 'p2p_advert_list' data
@@ -17,12 +19,14 @@ const useP2PAdvertList = (
 
         return advert_list.map(advert => ({
             ...advert,
+            /** Conversion rate from account currency to local currency, using current market rate if applicable. */
+            effective_rate: getRate(advert.local_currency || ''),
             /** Determine if the rate is floating or fixed */
             is_floating: advert.rate_type === 'float',
             /** The advert creation time in epoch. */
             created_time: new Date(advert.created_time),
         }));
-    }, [data?.p2p_advert_list?.list]);
+    }, [data?.p2p_advert_list?.list, getRate]);
 
     return {
         /** The 'p2p_advert_list' response. */
