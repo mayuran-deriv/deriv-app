f38b5a463b4b885b14248ed28af730ca70a58922
Wojciech Brygola <wojciech@regentmarkets.com>
2024-05-31 11:54:16 +0100

    feat: added subscription for linked options account

--- packages/wallets/src/components/DerivAppsSection/DerivAppsTradingAccount.tsx
+++ packages/wallets/src/components/DerivAppsSection/DerivAppsTradingAccount.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useActiveLinkedToTradingAccount, useActiveWalletAccount, useBalance } from '@deriv/api-v2';
+import {
+    useActiveLinkedToTradingAccount,
+    useActiveWalletAccount,
+    useBalanceSubscription,
+    useBalance,
+    useAuthorize,
+} from '@deriv/api-v2';
 import { LabelPairedArrowsRotateSmBoldIcon, LabelPairedArrowUpArrowDownSmBoldIcon } from '@deriv/quill-icons';
 import useDevice from '../../hooks/useDevice';
 import { WalletText } from '../Base';
 import { WalletListCardBadge } from '../WalletListCardBadge';
 import { WalletMarketIcon } from '../WalletMarketIcon';
+import { displayMoney } from '@deriv/api-v2/src/utils';
 
 const DerivAppsTradingAccount: React.FC = () => {
     const { isMobile } = useDevice();
@@ -13,6 +20,36 @@ const DerivAppsTradingAccount: React.FC = () => {
     const { isLoading } = useBalance();
     const { data: activeWallet } = useActiveWalletAccount();
     const { data: activeLinkedToTradingAccount } = useActiveLinkedToTradingAccount();
+    const { data: balanceData, subscribe, unsubscribe } = useBalanceSubscription();
+    const { data: authData } = useAuthorize();
+
+    useEffect(() => {
+        if (activeLinkedToTradingAccount) {
+            subscribe({
+                account: activeLinkedToTradingAccount.loginid,
+            });
+        }
+
+        return () => {
+            unsubscribe();
+        };
+    }, [activeLinkedToTradingAccount, subscribe]);
+
+    const linkedAccountCurrencyCode = activeLinkedToTradingAccount?.currency_config?.display_code;
+    const linkedAccountPreferredLanguage = authData?.preferred_language;
+    let linkedAccountFormattedBalance = '';
+
+    if (
+        balanceData &&
+        typeof balanceData?.balance === 'number' &&
+        linkedAccountCurrencyCode &&
+        linkedAccountPreferredLanguage
+    ) {
+        linkedAccountFormattedBalance = displayMoney(balanceData?.balance, linkedAccountCurrencyCode, {
+            fractional_digits: activeLinkedToTradingAccount?.currency_config?.fractional_digits,
+            preferred_language: linkedAccountPreferredLanguage,
+        });
+    }
 
     return (
         <div className='wallets-deriv-apps-section wallets-deriv-apps-section__border'>
@@ -28,7 +65,7 @@ const DerivAppsTradingAccount: React.FC = () => {
                     <div className='wallets-skeleton wallets-deriv-apps-balance-loader' />
                 ) : (
                     <WalletText size='sm' weight='bold'>
-                        {activeLinkedToTradingAccount?.display_balance}
+                        {linkedAccountFormattedBalance}
                     </WalletText>
                 )}
                 <WalletText color='less-prominent' lineHeight='sm' size='xs' weight='bold'>
